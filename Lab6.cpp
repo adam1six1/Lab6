8d2fa22d307f220708fe920faf689c1bcf70c8c3
Adam Khaled Malas <amalas@purdue.edu>
2024-02-14 11:56:21 -0500

    initial working

--- Lab6.cpp
+++ Lab6.cpp
@@ -20,14 +20,16 @@ int main() {
 	Image foregroundImage;
 	foregroundImage = foregroundTex.copyToImage();
 	Vector2u sz = backgroundImage.getSize();
+	Color green = foregroundImage.getPixel(0, 0);
 	for (int y = 0; y < sz.y; y++) {
 		for (int x = 0; x < sz.x; x++) {
-			// These two loops will run the code inside for each pixel in
-			the background image
-				// You can access the current pixel at x,y like so:
-				Color example = foregroundImage.getPixel(x, y);
-			// Color objects store the individual channel values like
-			example.r example.g and example.b
+			// These two loops will run the code inside for each pixel in the background image
+			// You can access the current pixel at x,y like so: Color example = foregroundImage.getPixel(x, y);
+			// Color objects store the individual channel values like example.r example.g and example.b
+
+			if (foregroundImage.getPixel(x, y) == green) {
+				foregroundImage.setPixel(x, y, backgroundImage.getPixel(x, y));
+			}
 		}
 	}
 	// By default, just show the foreground image
